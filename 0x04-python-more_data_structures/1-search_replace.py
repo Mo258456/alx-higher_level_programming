d788706bec13dad0fad8b7fc75c9f3abb4c38009
MoHack <60172765+Mo258456@users.noreply.github.com>
2021-11-24 22:08:45 -0500

    Upload commit for my projects trial one if they work

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if my_list is None:
+        return None
+
+    new = my_list.copy()
+
+    for i in new:
+        if i == search:
+            new[new.index(i)] = replace
+        else:
+            continue
+    return new
