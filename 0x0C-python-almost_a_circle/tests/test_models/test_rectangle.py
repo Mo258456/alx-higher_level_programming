60f5883ba21a3768485c447a8f76fc7f6a2c8ff6
MoHack <60172765+Mo258456@users.noreply.github.com>
2022-01-01 10:36:43 +0300

    uuu... what a mistake I made fix

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # test_rectangle.py
-# Brennan D Baraban <375@holbertonschool.com>
+# Moti Dawit moti.davis16@gmail.com
 """Defines unittests for models/rectangle.py.
 
 Unittest classes:
