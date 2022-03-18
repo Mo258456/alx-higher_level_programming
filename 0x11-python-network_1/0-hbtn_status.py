3b30a43ec0d5af30e56284b9d2739c570da00744
MoHack <moti.davis16@gmail.com>
2022-03-18 09:16:05 -0400

    Minor error fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+"""
 import urllib.request
 
-
 if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
+        read_res = res.read()
         print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+        print('\t - type: {}'.format(type(read_res)))
+        print('\t - content: {}'.format(read_res))
+        print('\t - utf8 content: {}'.format(read_res.decode('utf-8')))
+
