3b36500ad284eb09b79656f2fcdf12032f5f7e2d
MoHack <moti.davis16@gmail.com>
2022-04-13 10:28:30 -0400

    JavaScript WebScraping Corrections

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,13 +1,22 @@
 #!/usr/bin/node
+// Prints all characters of a Star Wars movie, with the episode's number passed
+// as argument
+
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
+const episode = process.argv[2];
+
+request('https://swapi-api.hbtn.io/api/films/' + episode, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const json = JSON.parse(body);
+    json.characters.forEach(character => {
+      request(character, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        } else {
+          const json = JSON.parse(body);
+          console.log(json.name);
         }
       });
     });
